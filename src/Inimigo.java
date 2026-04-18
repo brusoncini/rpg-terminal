f50d35645a2e73c865dfe0ec13f686f21f470ac7
Bruna Soncini <soncinibruna@gmail.com>
2026-04-18 13:11:00 -0300

    feat: add classe inimigo e combate simples (luta)

---
+++ src/Inimigo.java
@@ -0,0 +1,11 @@
+public class Inimigo {
+    String nome;
+    int vida;
+    int ataque;
+
+    public Inimigo(String nome, int vida, int ataque) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+    }
+}
--- src/Jogo.java
+++ src/Jogo.java
@@ -2,7 +2,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Jogo {
-
     Scanner leitor;
     boolean jogoRodando;
     Jogador jogador;
@@ -26,18 +25,24 @@ public class Jogo {
                     System.out.println("Saindo do jogo...");
                     this.jogoRodando = false;
                     break;
+
                 case 1:
                     this.novoJogo();
                     break;
+
                 case 2:
                     this.verStatus();
                     break;
+
                 case 3:
                     this.explorar();
                     break;
+
                 default:
                     System.out.println("Opção inválida.");
             }
+
+            System.out.println();
         }
     }
 
@@ -45,7 +50,7 @@ public class Jogo {
         System.out.println("=== RPG EM TERMINAL ===");
         System.out.println("1 - Novo jogo");
         System.out.println("2 - Ver status");
-        System.out.println("3 - Explorar!");
+        System.out.println("3 - Explorar");
         System.out.println("0 - Sair");
         System.out.print("Escolha: ");
     }
@@ -56,8 +61,8 @@ public class Jogo {
 
         this.jogador = new Jogador(nome);
 
-        System.out.println("Novo jogo iniciado com sucesso.");
-        System.out.println("Personagem criado: " + this.jogador.nome);
+        System.out.println("Novo jogo iniciado!");
+        System.out.println("Personagem criado com sucesso.");
     }
 
     public void verStatus() {
@@ -78,7 +83,10 @@ public class Jogo {
         int evento = this.aleatorio.nextInt(3);
 
         if (evento == 0) {
-            System.out.println("Um inimigo apareceu no caminho!");
+            Inimigo inimigo = new Inimigo("Goblin", 30, 8);
+            System.out.println("Um " + inimigo.nome + " apareceu no caminho!");
+            this.lutar(inimigo);
+
         } else if (evento == 1) {
             System.out.println("Você encontrou uma poção!");
             this.jogador.vida = this.jogador.vida + 20;
@@ -88,9 +96,34 @@ public class Jogo {
             }
 
             System.out.println("A vida de " + this.jogador.nome + " foi recuperada.");
+
         } else {
             System.out.println(this.jogador.nome + " andou bastante, mas não encontrou nada.");
         }
     }
-}
 
+    public void lutar(Inimigo inimigo) {
+        System.out.println("A batalha começou!");
+
+        while (this.jogador.vida > 0 && inimigo.vida > 0) {
+            System.out.println();
+            System.out.println(this.jogador.nome + " ataca o " + inimigo.nome + "!");
+            inimigo.vida = inimigo.vida - this.jogador.ataque;
+            System.out.println("O " + inimigo.nome + " ficou com " + inimigo.vida + " de vida.");
+
+            if (inimigo.vida <= 0) {
+                System.out.println(this.jogador.nome + " venceu a batalha!");
+                break;
+            }
+
+            System.out.println("O " + inimigo.nome + " atacou " + this.jogador.nome + "!");
+            this.jogador.vida = this.jogador.vida - inimigo.ataque;
+            System.out.println(this.jogador.nome + " ficou com " + this.jogador.vida + " de vida.");
+
+            if (this.jogador.vida <= 0) {
+                System.out.println(this.jogador.nome + " foi derrotado.");
+                this.jogoRodando = false;
+            }
+        }
+    }
+}
